46cd9b7e806bcc24f08e15a948876266cb6309ae
paolo-magnani <paolo.magnani@studenti.unimi.it>
2023-11-14 10:18:56 +0100

    improved simulated annealing, minor bugs fixed

--- Es08/es8.cpp
+++ Es08/es8.cpp
@@ -1,129 +1,9 @@
-#include "../libremia.h"
+#include "es8.h"
 #define M 100000
 #define N 100
 
-
-double psiplus(const double & x, const double & mu, const double & sigma){
-	return exp(-0.5*pow((x+mu)/sigma, 2.));
-}
-
-double psiminus(const double & x, const double & mu, const double & sigma){
-	return exp(-0.5*pow((x-mu)/sigma, 2.));
-}
-
-double psi(const double & x, const double & mu, const double & sigma){
-	return psiminus(x,mu,sigma)+psiplus(x,mu,sigma);;
-}
-
-
-double Ham(const double & x, const double & mu, const double & sigma){
-	double V = (((x*x)-2.5)*x*x);
-	double K = -0.5*(mu*mu + (x*x)- (sigma*sigma)+ (2*mu*x*(psiplus(x, mu, sigma)-psiminus(x, mu, sigma))/psi(x, mu, sigma)))*pow(1./sigma,4.);
-	return V+K;
-}
-
-double Boltz(const double & E, const double & beta){
-	return exp(-beta*E);
-	
-} 
-
-double accept(const double ratio){
-	if(ratio<1.) return ratio;
-	else return 1.;
-}
-
-
-double Eval_H(const unsigned int& B, const double& step, const double& mu, const double& sigma, Random& rand){
-
-	double E=0;
-	double xi=0., psi_old, psi_new, ratio;
-	
-	for(unsigned int i=0; i<B; i++){
-			
-			psi_old = pow(psi(xi,mu,sigma),2.);
-			
-			double x_old = xi; 
-			
-			E += Ham(xi, mu, sigma);
-			
-			xi = xi + step*rand.Rannyu(-1.,1.);
-			
-			psi_new = pow(psi(xi,mu,sigma),2.);
-			
-			if(psi_old == 0) ratio=1.;
-			else ratio = psi_new/psi_old;
-			
-			double alpha = accept(ratio);
-			if(rand.Rannyu()>alpha) xi=x_old;
-	
-	}
-
-	return E/double(B);
-
-} 
-
-
-void Stima_Finale(const unsigned int& B, double& step, const double& mu, const double& sigma, vector<double>& E, Random& rand){
-
-	double xi=0., psi_old, psi_new, ratio;
-	double acc = 0, acc_old;
-
-	do{
-		acc_old = acc;
-		acc = 0, xi=0;	
-		for(unsigned int i=0; i<100; i++){
-			
-			psi_old = pow(psi(xi,mu,sigma),2.);
-			
-			double x_old = xi;			
-			xi = xi + step*rand.Rannyu(-1.,1.);
-			
-			psi_new = pow(psi(xi,mu,sigma),2.);
-			
-			if(psi_old == 0) ratio=1.;
-			else ratio = psi_new/psi_old;
-			
-			double alpha = accept(ratio);
-			if(rand.Rannyu()<=alpha) acc++;
-			else xi=x_old;
-	
-		}
-		acc /= double(100);
-		if(acc<acc_old) step -= 0.1;
-		else step += 0.1;
-		cout << "acceptance: " << acc << endl;
-		cout << "step: " << step << endl;
-	}while(acc<0.4 or acc>0.6);
-	
-	acc = 0;
-	for(unsigned int i=0; i<B; i++){
-			
-			psi_old = pow(psi(xi,mu,sigma),2.);
-			
-			double x_old = xi; 
-			
-			E[i]= Ham(xi, mu, sigma);
-			
-			xi = xi + step*rand.Rannyu(-1.,1.);
-			
-			psi_new = pow(psi(xi,mu,sigma),2.);
-			
-			if(psi_old == 0) ratio=1.;
-			else ratio = psi_new/psi_old;
-			
-			double alpha = accept(ratio);
-			if(rand.Rannyu()<=alpha) acc++;
-			else xi=x_old;
-	
-	}
-	acc /= double(B);
-}
-
-
-
 int main(int argc, char** argv){
-	
-	const unsigned int L=M/N;
+
 	double step=1;
 
 	if(argc<2)	cout << " <nstep> not specified, using default step instead" << endl;
@@ -131,21 +11,19 @@ int main(int argc, char** argv){
 	
 	Random rand;
 	iniz(rand);
-	ofstream out_beta, out_par, out_H, out_histo;
+	ofstream out_beta, out_par, out_histo;
 	out_beta.open("Beta.dat");
 	out_par.open("Parameters.dat");
-	out_H.open("Energy.dat");
 	
-	double psi_old, psi_new, ratio, weight_old, weight_new;
+	double ratio, weight_old, weight_new;
 	double mu=rand.Rannyu(), sigma=rand.Rannyu();
-	double xi=0.;
 	
 	vector<double> E(M, 0.), mean(N, 0.), err(N, 0.);
 	
 	//Simulated Annealing
 	for(double beta=1; beta<=1000; beta+=3){ //slowly freezing
 		
-		unsigned int Tstep=1000/beta;
+		unsigned int Tstep=1000/sqrt(beta);
 	
 		//MU, SIGMA
 		for(unsigned int i=0; i<Tstep; i++){
@@ -187,11 +65,10 @@ int main(int argc, char** argv){
 	Stima_Finale(M, step, mu, sigma, E, rand);
 	mediablocchi(M, N, E, mean, err);
 
-	for(unsigned int i=0; i<N; i++) out_H << i+1 << "," << mean[i] << "," << err[i] << endl;
+	stampamedia("Energy.dat",M, mean,err);
 
 	out_beta.close();
 	out_par.close();
-	out_H.close();
 	
 	return 0;
 }
---
+++ Es08/es8.h
@@ -0,0 +1,118 @@
+#include "../libremia.h"
+
+
+double psiplus(const double & x, const double & mu, const double & sigma){ // parte della funzione di prova del ground state
+	return exp(-0.5*pow((x+mu)/sigma, 2.));
+}
+
+double psiminus(const double & x, const double & mu, const double & sigma){ // parte della funzione di prova del ground state
+	return exp(-0.5*pow((x-mu)/sigma, 2.));
+}
+
+double psi(const double & x, const double & mu, const double & sigma){ // funzione di prova per il ground state
+	return psiminus(x,mu,sigma)+psiplus(x,mu,sigma);;
+}
+
+
+double Ham(const double & x, const double & mu, const double & sigma){ // calcola l'Hamiltoniana del sistema
+	double V = (((x*x)-2.5)*x*x);
+	double K = -0.5*(mu*mu + (x*x)- (sigma*sigma)+ (2*mu*x*(psiplus(x, mu, sigma)-psiminus(x, mu, sigma))/psi(x, mu, sigma)))*pow(1./sigma,4.);
+	return V+K;
+}
+
+double Boltz(const double & E, const double & beta){ // calcola il peso di Boltzmann
+	return exp(-beta*E);
+	
+} 
+
+double accept(const double ratio){ // restituisce la probabilità di accettazione di una mossa del Metropolis
+	if(ratio<1.) return ratio;
+	else return 1.;
+}
+
+
+double Eval_H(const unsigned int& B, const double& step, const double& mu, const double& sigma, Random& rand){ // stima il valor medio di H 
+
+	double E=0;
+	double xi=0., psi_old, psi_new, ratio;
+	
+	for(unsigned int i=0; i<B; i++){
+			
+			psi_old = pow(psi(xi,mu,sigma),2.);
+			
+			double x_old = xi; 
+			
+			E += Ham(xi, mu, sigma);
+			
+			xi = xi + step*rand.Rannyu(-1.,1.);
+			
+			psi_new = pow(psi(xi,mu,sigma),2.);
+			
+			if(psi_old == 0) ratio=1.;
+			else ratio = psi_new/psi_old;
+			
+			double alpha = accept(ratio);
+			if(rand.Rannyu()>alpha) xi=x_old;
+	
+	}
+
+	return E/double(B);
+
+} 
+
+
+void Stima_Finale(const unsigned int& B, double& step, const double& mu, const double& sigma, vector<double>& E, Random& rand){ // stima il valore di H nel ground state dopo aver trovato il valore di mu e sigma
+
+	double xi=0., psi_old, psi_new, ratio;
+	double acc = 0, acc_old;
+
+	do{
+		acc_old = acc;
+		acc = 0, xi=0;	
+		for(unsigned int i=0; i<100; i++){
+			
+			psi_old = pow(psi(xi,mu,sigma),2.);
+			
+			double x_old = xi;			
+			xi = xi + step*rand.Rannyu(-1.,1.);
+			
+			psi_new = pow(psi(xi,mu,sigma),2.);
+			
+			if(psi_old == 0) ratio=1.;
+			else ratio = psi_new/psi_old;
+			
+			double alpha = accept(ratio);
+			if(rand.Rannyu()<=alpha) acc++;
+			else xi=x_old;
+    
+		}
+		acc /= double(100);
+		if(acc<acc_old) step -= 0.1;
+		else step += 0.1;
+		cout << "acceptance: " << acc << endl;
+		cout << "step: " << step << endl;
+	}while(acc<0.4 or acc>0.6);
+	
+	acc = 0;
+	for(unsigned int i=0; i<B; i++){
+			
+			psi_old = pow(psi(xi,mu,sigma),2.);
+			
+			double x_old = xi; 
+			
+			E[i]= Ham(xi, mu, sigma);
+			
+			xi = xi + step*rand.Rannyu(-1.,1.);
+			
+			psi_new = pow(psi(xi,mu,sigma),2.);
+			
+			if(psi_old == 0) ratio=1.;
+			else ratio = psi_new/psi_old;
+			
+			double alpha = accept(ratio);
+			if(rand.Rannyu()<=alpha) acc++;
+			else xi=x_old;
+	
+	}
+	acc /= double(B);
+}
