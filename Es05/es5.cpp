c34498c48c36bfce16d0bca7670705e0168cb27d
paolo-magnani <paolo.magnani@studenti.unimi.it>
2023-11-14 08:34:00 +0100

    nuove modifiche

--- Es05/es5.cpp
+++ Es05/es5.cpp
@@ -1,29 +1,29 @@
 #include "es5.h"
-#define M 1000000
-#define N 100
+#define M 1000000 // numero di step per il Metropolis
+#define N 100 // numero di blocchi
 
 
 int main(){
 
 	input myinput;
 
-	ReadInput(myinput);
+	ReadInput(myinput); // prendo variabili di input dal file input.in
 
 	Random rand;
-	iniz(rand);
+	iniz(rand); // inizializzo il generatore di numeri casuali
 	ofstream outpsi;
 	outpsi.open(myinput.psi_data);
 	
 	double psi_old, psi_new, ratio, alpha;
-	int acc=0; 
+	int acc=0; // inizializzo l'accettanza a 0
 	double xi=myinput.xi, yi=myinput.yi, zi=myinput.zi;
 	vector<double> x(M,0.), y(M,0.), z(M,0.), r(M,0.), media(N, 0.), sigma(N, 0.);
 	
 	
-	for(unsigned int i=0; i<M; i++){
+	for(unsigned int i=0; i<M; i++){ // implementazione dell'algoritmo di Metropolis
 		
 		if(myinput.psi==true){
-			psi_old = pow(psi210(xi,yi,zi),2.);
+			psi_old = pow(psi210(xi,yi,zi),2.); // densità di probabilità del punto di partenza
 		}else{
 			psi_old = pow(psi100(xi,yi,zi),2.);
 		}
@@ -31,9 +31,9 @@ int main(){
 		x[i]=xi;
 		y[i]=yi;
 		z[i]=zi;
-		r[i]=radius(xi,yi,zi);
+		r[i]=radius(xi,yi,zi); // registra il valore della posizione
 		
-		if(myinput.Gauss==false){
+		if(myinput.Gauss==false){ // tento una mossa in una generica direzione e secondo una distribuzione definita (uniforme o gaussiana)
 			xi = xi + myinput.step*rand.Rannyu(-1.,1.);
 			yi = yi + myinput.step*rand.Rannyu(-1.,1.);
 			zi = zi + myinput.step*rand.Rannyu(-1.,1.);
@@ -43,34 +43,34 @@ int main(){
 			zi = rand.Gauss(zi,myinput.step);
 		}
 		
-		if(myinput.psi==true){
+		if(myinput.psi==true){ // dalla nuova posizione calcolo la distribuzione di probabilità
 			psi_new = pow(psi210(xi,yi,zi),2.);
 		}else{
 			psi_new = pow(psi100(xi,yi,zi),2.);
 		}
 		
 		
-		if(psi_old == 0) ratio=1;
+		if(psi_old == 0) ratio=1; // per evitare errori, la mossa è sempre accettata quando la posizione vecchia dà probabilità = 0
 		else ratio = psi_new/psi_old;
 		
 		alpha = accept(ratio);
-		if(rand.Rannyu()<=alpha){
+		if(rand.Rannyu()<=alpha){ // accetta la mossa secondo una probabilità che dipende la rapporto tra le densità di probabilità
 			acc++;
 			outpsi << xi << "," << yi << "," << zi << endl;
 			}
-		else{
+		else{ // altrimenti rimane nella stessa posizione
 			xi=x[i];
 			yi=y[i];
 			zi=z[i];
 		}
 		
 	}
-	mediablocchi(M, N, r, media, sigma);
+	mediablocchi(M, N, r, media, sigma); // calcolo il valor medio del raggio
 
-	stampamedia(myinput.mean_data, M, media, sigma);
+	stampamedia(myinput.mean_data, M, media, sigma); // stampo dati su file
 
 	cout << "Mean value: " << media[N-1] << endl;
-	cout << "Acceptance rate: " << double(acc)/double(M) << endl;
+	cout << "Acceptance rate: " << double(acc)/double(M) << endl; // stampo a video il valore dell'accettanza
 	outpsi.close();
 
 	
--- Es05/es5.h
+++ Es05/es5.h
@@ -1,15 +1,15 @@
 # include "../libremia.h"
 
-struct input{
-	bool Gauss;
-	bool psi;
-	double step;
-    double xi, yi, zi;
-	string mean_data;
+struct input{ //struct di input
+	bool Gauss; // = 1 se si estraggono numeri da una distribuzione gaussiana, = 0 se si estraggono da una distribuzione uniforme 
+	bool psi; // = 1 per lo stato 2p, = 0 per lo stato 1s
+	double step; // step per avere l'accettanza a ~0.5
+    double xi, yi, zi; // posizione iniziale
+	string mean_data; // nome file di output
 	string psi_data;
 };
 
-void ReadInput(input& myinput){
+void ReadInput(input& myinput){ // funzione che legge i file di input
 
     ifstream in;
     in.open("input.in");
@@ -26,17 +26,17 @@ void ReadInput(input& myinput){
     in.close();
 }
 
-double radius(const double& x, const double& y, const double& z){
+double radius(const double& x, const double& y, const double& z){ // restituisce il valore del raggio date le tre coordinate cartesiane
 	return sqrt((x*x)+(y*y)+(z*z));
 }
 
-double psi100(const double& x, const double& y, const double& z){
+double psi100(const double& x, const double& y, const double& z){ // restituisce la funzione di probabilità dell'orbitale 1 0 0
 	double psi = exp(-sqrt((x*x)+(y*y)+(z*z)))/(sqrt(M_PI));
 	return psi;
 }
 
 
-double psi210(const double& x, const double& y, const double& z){
+double psi210(const double& x, const double& y, const double& z){ // restituisce la funzione di probabilità dell'orbitale 2 1 0
 	double r = sqrt((x*x)+(y*y)+(z*z));
 	if(r==0) return 0;
 	
@@ -48,7 +48,7 @@ double psi210(const double& x, const double& y, const double& z){
 }
 
 
-double accept(const double ratio){
+double accept(const double ratio){ // restituisce la probabilità di accettazione di una mossa
 	if(ratio<1.) return ratio;
 	else return 1.;
 }
